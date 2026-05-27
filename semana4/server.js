1ed03161584fa91189a415957dc86cd00bf9749a
Valerin Michell Ibarguen Rivas <alixrosalbarivasmoreno@gmail.com>
2026-05-26 19:04:40 -0500

    Actividad semana 4

--- semana4/server.js
+++ semana4/server.js
@@ -1,12 +1,8 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando correctamente');
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando correctamente");
 });
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log("Servidor ejecutándose en el puerto 3000");
 });
